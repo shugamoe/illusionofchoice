32f54fe6452b0a44cb0632a23110c4ba38a88cbf
Julian McClellan <juliancmcclellan@gmail.com>
2020-03-10 13:50:21 -0500

    drake plan now builds workflowr site but does not prematurely publish

--- R/plan.R
+++ R/plan.R
@@ -1,5 +1,5 @@
 plan  <- drake_plan(
-  aboutWfr = wflow_publish(knitr_in("analysis/about.Rmd"), verbose = T),
-  licenseWfr = wflow_publish(knitr_in("analysis/license.Rmd"), verbose = T),
-  indexWfr = wflow_publish(knitr_in("analysis/index.Rmd"), view = T, verbose = T)
+  aboutWfr = wflow_build(knitr_in("analysis/about.Rmd"), verbose = T),
+  licenseWfr = wflow_build(knitr_in("analysis/license.Rmd"), verbose = T),
+  indexWfr = wflow_build(knitr_in("analysis/index.Rmd"), view = T, verbose = T)
 )
