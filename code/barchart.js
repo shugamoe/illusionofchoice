6743df1a54f61ef278ef9d27fa7fd34c09f3f606
Julian McClellan <juliancmcclellan@gmail.com>
2020-03-10 19:49:18 -0500

    basic derping around in d3 is useful. update to package lock

--- code/barchart.js
+++ code/barchart.js
@@ -1,11 +1,32 @@
 // !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
 
+height = 2 * height
 var barHeight = Math.floor(height / data.length);
 
-svg.selectAll('rect')
-  .data(data)
-  .enter().append('rect')
-    .attr('width', function(d) { return d * width; })
-    .attr('height', barHeight)
-    .attr('y', function(d, i) { return i * barHeight; })
-    .attr('fill', 'steelblue');
+bar = svg.appendMany('g', data)
+         .translate((d, i) => [0, i * barHeight]);
+
+bar.appendMany('rect', data)
+   .attr('width', d => .8 * width)
+   .attr('height', barHeight)
+   .attr('y', (d, i) => i * barHeight)
+   .attr('fill', 'steelblue');
+
+bar.append("text")
+  .attr("x", 0 )
+  .attr("y", barHeight / 2)
+  .attr("dy", ".35em")
+  .text(d => d.brandName);
+
+// d3-jetpack derp level fiddling
+// svg.selectAll('rect')
+   // .data(data)
+   // .enter().append('rect')//.appendMany('rect', data)
+     // .at((d, i) => {width: .8 * width,
+                    // height: barHeight,
+                    // y: i * barHeight,
+                    // fill: 'steelblue'
+  // });
+//
+// d3.select("body").append("div")
+    // .attrs(function(d, i) { return {title: d.title, id: "id-" + i}; });
